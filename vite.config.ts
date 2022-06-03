57316ee2cc5b20efa6216b90b3908863fa9b9d40
Andrew Hampton <andrew.hampton@switchthink.com>
2022-06-03 12:16:46 -0700

    include vuetify plugin in vite config via require

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as vuetify from '@vuetify/vite-plugin'
 
+const vuetify = require('@vuetify/vite-plugin')
 const path = require('path')
 
 // https://vitejs.dev/config/
